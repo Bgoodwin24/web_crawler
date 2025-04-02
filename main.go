98a7de5119ff84b99fb612f31bc4a782099faafc
Bgoodwin24 <bgoodwin24@gmail.com>
2025-04-01 19:12:04 -0700

    feat: Implement CLI argument validation
    - Update `main.go` to validate the correct number of provided CLI arguments
    - Display appropriate error messages for no arguments or too many arguments
    - Print a message when one argument is provided

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args
+	if len(args)-1 < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+
+	if len(args)-1 > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	if len(args)-1 == 1 {
+		fmt.Printf("starting crawl of: %s", args[1])
+	}
 }
